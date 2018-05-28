7e00b18314fbfc3f1d511b416798093bc3fe2da3
nprassas01@manhttan.edu <nprassas01@manhattan.edu>
2018-05-28 14:24:15 -0400

    implement methods in Extract.java

--- ps1/src/twitter/Extract.java
+++ ps1/src/twitter/Extract.java
@@ -3,8 +3,10 @@
  */
 package twitter;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.time.Instant;
 
 /**
  * Extract consists of methods that extract information from a list of tweets.
@@ -24,7 +26,58 @@ public class Extract {
      *         every tweet in the list.
      */
     public static Timespan getTimespan(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        /* If the list is empty, return a Timespan with arbitrary 
+         * start and end time stamps that are identical.
+         */
+        if (tweets.isEmpty()) {
+            String timeStampString = "2016-02-17T10:00:00Z";
+            return new Timespan(Instant.parse(timeStampString), 
+                                Instant.parse(timeStampString));
+        }
+        
+        Instant start = getMinTimeStamp(tweets);
+        Instant end = getMaxTimeStamp(tweets);
+        return new Timespan(start, end);
+    }
+    
+    /**
+     * 
+     * @param tweets requires tweets.size() > 0
+     *        tweets list is not modified by this method.
+     * @return the earliest chronological timestamp contained within any of the tweets
+     */
+    private static Instant getMinTimeStamp(final List<Tweet> tweets) {
+        assert tweets.size() > 0;
+        Instant minTimeStamp = Instant.MAX;
+        
+        for (Tweet tweet: tweets) {
+            Instant tweetTimeStamp = tweet.getTimestamp();
+            if (minTimeStamp.isAfter(tweetTimeStamp)) {
+                minTimeStamp = tweetTimeStamp;
+            }
+        }
+        
+        return minTimeStamp;
+    }
+    
+    /**
+     * 
+     * @param tweets requires tweets.size() > 0
+     *        tweets list is not modified by this method.
+     * @return the latest chronological timestamp contained with any of the tweets
+     */
+    private static Instant getMaxTimeStamp(final List<Tweet> tweets) {
+        assert tweets.size() > 0;
+        Instant maxTimeStamp = Instant.MIN;
+        
+        for (Tweet tweet: tweets) {
+            Instant tweetTimeStamp = tweet.getTimestamp();
+            if (maxTimeStamp.isBefore(tweetTimeStamp)) {
+                maxTimeStamp = tweetTimeStamp;
+            }
+        }
+        
+        return maxTimeStamp;
     }
 
     /**
@@ -43,7 +96,68 @@ public class Extract {
      *         include a username at most once.
      */
     public static Set<String> getMentionedUsers(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+        Set<String> mentionedUsers = new HashSet<>();
+        
+        for (Tweet t: tweets) {
+            Set<String> subsetMentionedUsers = getMentionedUsersInText(t.getText());
+            mentionedUsers.addAll(subsetMentionedUsers);
+        }
+        
+        return mentionedUsers;
+    }
+    
+    /**
+     * Get the set of valid usernames contained within one string of text.
+     * @param text a string of text, ideally from a tweet but not required.
+     * @return the set of valid usernames contained within the string of text.
+     *         promise: All usernames in the set will have all alphabetic letters in lower-case form.
+     */
+    private static Set<String> getMentionedUsersInText(String text) {
+        Set<String> mentionedUsers = new HashSet<>();
+        
+        int i = 0;
+        int textLen = text.length();
+        
+        while (i < textLen) {
+            char c = text.charAt(i);
+            
+            /* set prevChar to an arbitrary non-valid twitter username char if i = 0
+             * or simply the previous character if i > 0.
+             */
+            char prevChar = i == 0 ? '@' : text.charAt(i - 1);
+            
+            if (c == '@' && !validTwitterUserChar(prevChar)) {
+                ++i;
+                StringBuilder sb = new StringBuilder();
+                while (i < textLen && validTwitterUserChar(text.charAt(i))) {
+                    sb.append(text.charAt(i));
+                    ++i;
+                }
+                
+                /* Add the username with all letters converted to lower-case */
+                if (sb.length() > 0) {
+                    mentionedUsers.add(sb.toString().toLowerCase());
+                }
+            }
+            
+            ++i;
+        }
+        
+        return mentionedUsers;
+    }
+    
+    /**
+     * Determine whether or not a given character is a character that
+     * can be part of a valid twitter username.  A valid character
+     * in a twitter username is either a letter, digit, hyphen, or underscore.
+     * @param c
+     * @return
+     */
+    private static boolean validTwitterUserChar(char c) {
+        if (c == '-' || c == '_' ||
+            Character.isDigit(c) || Character.isLetter(c)) return true;
+        
+        return false;
     }
 
 }
--- ps1/test/twitter/ExtractTest.java
+++ ps1/test/twitter/ExtractTest.java
@@ -6,7 +6,10 @@ package twitter;
 import static org.junit.Assert.*;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -14,6 +17,27 @@ import org.junit.Test;
 public class ExtractTest {
 
     /*
+     * Testing Strategy:
+     * 
+     * Break input list of tweets for getTimeSpan into the following cases:
+     * list of two tweets with two different times
+     * non-empty list of  six tweets with different times shuffled randomly
+     * empty list of tweets in which the time interval should be of length 0
+     *
+     * 
+     * Break input list of tweets for getMentioned users into following partitions:
+     * empty list of tweets
+     * tweets without any usernames
+     * valid twitter usernames within text ex; @henry__899-87
+     * invalid twitter usernames within text that have the @ character preceeding the name
+     *                 ex; bitdiddle@mit.edu
+     * twitter usernames that are valid up to a certain point, 
+     *         ex; @john45()()) --> john45 would be the extracted username
+     * same username mentioned with different cases for some or all characters
+     * valid username at start of tweet
+     * valid username at end of tweet
+     * valid username in middle of tweet
+     * 
      * TODO: your testing strategies for these methods should go here.
      * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
      * Make sure you have partitions.
@@ -21,9 +45,18 @@ public class ExtractTest {
     
     private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
     private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+    private static final Instant d3 = Instant.parse("2016-05-17T11:00:00Z");
+    private static final Instant d4 = Instant.parse("2016-05-17T11:01:02Z");
+    private static final Instant d5 = Instant.parse("2016-05-17T11:01:04Z");
+    private static final Instant d6 = Instant.parse("2016-12-17T11:00:00Z");
     
     private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
     private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
+    private static final Tweet tweet3 = new Tweet(2, "jane", "where is the juice #thirsty", d3);
+    private static final Tweet tweet4 = new Tweet(2, "hansel", "here we go again #mad", d4);
+    private static final Tweet tweet5 = new Tweet(2, "roger", "lets go cavaliers!", d5);
+    private static final Tweet tweet6 = new Tweet(2, "benny", "wishing everyone a wonderful christmas", d6);
+    
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
@@ -39,10 +72,102 @@ public class ExtractTest {
     }
     
     @Test
+    public void testGetTimeSpanSixTweets() {
+        List<Tweet> tweetList = Arrays.asList(tweet1, tweet2, tweet3, 
+                                              tweet4, tweet5, tweet6);
+        
+        Collections.shuffle(tweetList);
+        Timespan timespan = Extract.getTimespan(tweetList);
+        
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d6, timespan.getEnd());
+    }
+    
+    /* For this test the start and end time stamps should be equal, since
+     * zero tweets can fit into a time interval of no time at all.
+     */
+    @Test
+    public void testGetTimeSpanZeroTweets() {
+        List<Tweet> list = new ArrayList<>();
+        Timespan timespan = Extract.getTimespan(list);
+        
+        assertNotNull("should not be null", timespan);
+        Instant start = timespan.getStart();
+        Instant end = timespan.getEnd();
+        assertEquals("expected start and end timestamps to be the same", end, start);
+    }
+    
+    @Test
     public void testGetMentionedUsersNoMention() {
         Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
         
         assertTrue("expected empty set", mentionedUsers.isEmpty());
+    }
+    
+    @Test
+    public void testGetMentionedUsersEmptyList() {
+        List<Tweet> emptyList = new ArrayList<>();
+        Set<String> mentionedUsers = Extract.getMentionedUsers(emptyList);
+        assertTrue("expected empty set", mentionedUsers.isEmpty());
+    }
+    
+    @Test
+    public void testGetMentionedStartOfText() {
+        Tweet tweetA = new Tweet(1, "alyssa", "@mikey why did you unfollow me?", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA));
+        assertEquals("expected size of set", 1, mentionedUsers.size());
+        assertTrue(mentionedUsers.contains("mikey"));
+    }
+    
+    @Test
+    public void testGetMentionedEndOfText() {
+        Tweet tweetA = new Tweet(1, "alyssa", "happy new year bro @mikey", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA));
+        assertEquals("expected size of set", 1, mentionedUsers.size());
+        assertTrue(mentionedUsers.contains("mikey"));
+    }
+    
+    @Test
+    public void testGetMentionedMiddleOfText() {
+        Tweet tweetA = new Tweet(1, "alyssa", "happy birthdy to @mikey my good buddy.", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA));
+        assertEquals("expected size of set", 1, mentionedUsers.size());
+        assertTrue(mentionedUsers.contains("mikey"));
+    }
+    
+    @Test
+    public void testGetMentionedUsersInvalidUsernames() {
+        Tweet tweetA = new Tweet(1, "alyssa", "happy birthdy to hero@mikey my good buddy.", d1);
+        Tweet tweetB = new Tweet(1, "alyssa", "bitdiddle@mit.edu is a good student", d1);
+        Tweet tweetC = new Tweet(1, "alyssa", "happy birthday thisguy@johnnboy", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA, tweetB, tweetC));
+        assertTrue("expected empty set", mentionedUsers.isEmpty());
+    }
+    
+    @Test
+    public void testGetMentionedUsersNoDuplicates() {
+        Tweet tweetA = new Tweet(1, "alyssa", "happy birthdy to @mikey-123_8 my good buddy.", d1);
+        Tweet tweetB = new Tweet(1, "alyssa", "hey @MikeY-123_8 what is up", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA, tweetB));
+        assertEquals("expected size of set", 1, mentionedUsers.size());
+        assertTrue(mentionedUsers.contains("mikey-123_8") || mentionedUsers.contains("MikeY-123_8"));
+    }
+    
+    @Test
+    public void testGetMentionedUsersValidWithTrailingInvalidChars() {
+        Tweet tweetA = new Tweet(1, "alyssa", "waiting for @tim-234()() to arrive", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA));
+        assertEquals("expected size of set", 1, mentionedUsers.size());
+        assertTrue(mentionedUsers.contains("tim-234"));
+    }
+    
+    @Test
+    public void testGetMentionedUsersMultipleUsersInSingleTweet() {
+        // james-7 should not be extracted because the @ before username is preceded by "hey"
+        Tweet tweetA = new Tweet(1, "alyssa", "@tim_34 hey@james-7 @_mulan", d1);
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweetA));
+        assertEquals("expected size of set", 2, mentionedUsers.size());
+        assertTrue(mentionedUsers.contains("tim_34") && mentionedUsers.contains("_mulan"));
     }
 
     /*
