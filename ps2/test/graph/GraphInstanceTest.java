ddb578fe46c716a64e8881a7e7083b81bf101d3e
nprassas01@manhttan.edu <nprassas01@manhattan.edu>
2018-06-03 21:16:36 -0400

    write unit tests for GraphInstanceTest class

--- ps2/test/graph/GraphInstanceTest.java
+++ ps2/test/graph/GraphInstanceTest.java
@@ -6,6 +6,8 @@ package graph;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -20,8 +22,28 @@ import org.junit.Test;
 public abstract class GraphInstanceTest {
     
     // Testing strategy
-    //   TODO
-    
+    // 
+    // Partition for add():
+    // empty, non-empty
+    // vertices that exist in graph beforehand, don't exist in graph beforehand
+    // 
+    //
+    // Partition for set():
+    // vertices that exist in graph beforehand, don't exist in graph beforehand
+    // weight = 0, weight > 0
+    //
+    // Partition for remove():
+    // vertices existent in the graph, non-existent in the graph
+    // vertices with other edges, without other edges
+    // 
+    // Partition for vertices():
+    // number of vertices is 0, > 0
+    // 
+    // Partition for sources():
+    // number of edges pointing toward target is 0, > 0
+    //
+    // Partition for targets():
+    // number of edges pointing out from source is 0, > 0
     /**
      * Overridden by implementation-specific test classes.
      * 
@@ -42,5 +64,129 @@ public abstract class GraphInstanceTest {
     }
     
     // TODO other tests for instance methods of Graph
+    @Test
+    public void testAddToEmptyGraph() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("hello"));
+    }
     
+    @Test
+    public void testAddToNonEmptyGraph() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("hello"));
+        assertTrue(graph.add("goodbye"));
+    }
+    
+    @Test
+    public void testAddAlreadyExistingVertex() {
+        Graph<String> graph = emptyInstance();
+        graph.add("hello");
+        assertFalse(graph.add("hello"));
+    }
+    
+    @Test
+    public void testRemoveEmptyGraph() {
+        Graph<String> g = emptyInstance();
+        assertFalse(g.remove("jim"));
+    }
+    
+    @Test
+    public void testVerticesEmptyGraph() {
+        assertEquals(0, emptyInstance().vertices().size());
+    }
+    
+    @Test
+    public void testVerticesNonEmptyGraph() {
+        Graph<String> g = emptyInstance();
+        g.add("roger");
+        g.add("peanut");
+        
+        Set<String> vertices = g.vertices();
+        
+        assertEquals(2, vertices.size());
+        assertTrue(vertices.contains("roger"));
+        assertTrue(vertices.contains("peanut"));
+    }
+    
+    @Test
+    public void testSetNewEdge() {
+        Graph<String> g = emptyInstance();
+        g.add("roger");
+        g.add("peanut");
+        
+        assertEquals(0, g.set("roger", "peanut", 23));
+    }
+    
+    @Test
+    public void testSetChangeEdgeWeight() {
+        Graph<String> g = emptyInstance();
+        g.add("roger");
+        g.add("peanut");
+        
+        g.set("roger", "peanut", 23);
+        assertEquals(23, g.set("roger", "peanut", 5));
+    }
+    
+    @Test
+    public void testSetNonExistingVertex() {
+        Graph<String> g = emptyInstance();
+        g.add("roger");
+        // "sam" vertex doesn't exist but should get created in set below
+        assertEquals(0, g.set("roger", "sam", 56));
+    }
+    
+    @Test
+    public void testSetDeleteEdge() {
+        Graph<String> g = emptyInstance();
+        g.set("roger", "sam", 40);
+        assertEquals(40, g.set("sam", "roger", 0));
+        assertEquals(0, g.set("sam", "roger", 3));
+    }
+    
+    @Test
+    public void testSourcesEmptyGraph() {
+        assertEquals(0, emptyInstance().sources("tom").size());
+    }
+    
+    @Test
+    public void testSourcesNoSources() {
+        Graph<String> g = emptyInstance();
+        g.set("roger", "sam", 40);
+        Map<String, Integer> sourcesToRoger = g.sources("roger");
+        assertEquals(0, sourcesToRoger.size());
+    }
+    
+    @Test
+    public void testSourcesSomeSources() {
+        Graph<String> g = emptyInstance();
+        g.set("roger", "sam", 40);
+        g.set("jim", "sam", 90);
+        Map<String, Integer> sourcesMap = g.sources("sam");
+        assertEquals(2, sourcesMap.size());
+        assertEquals(40, (int) sourcesMap.get("roger"));
+        assertEquals(40, (int) sourcesMap.get("jim"));
+    }
+    
+    @Test
+    public void testTargetsEmptyGraph() {
+        assertEquals(0, emptyInstance().targets("tom").size());
+    }
+    
+    @Test
+    public void testTargetsNoTargets() {
+        Graph<String> g = emptyInstance();
+        g.set("roger", "sam", 40);
+        assertEquals(0, g.targets("sam").size());
+    }
+    
+    @Test
+    public void testTargetsSomeTargets() {
+        Graph<String> g = emptyInstance();
+        g.set("roger", "sam", 40);
+        g.set("roger", "april", 30);
+        Map<String, Integer> targetsMap = g.targets("roger");
+        assertEquals(2, targetsMap.size());
+        assertEquals(40, (int) targetsMap.get("sam"));
+        assertEquals(30, (int) targetsMap.get("april"));
+    }
 }
